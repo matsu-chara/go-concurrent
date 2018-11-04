cdf5cb02d7f2aeee6754b2433ecef826e01cb7d2
matsu-chara <matsuy00@gmail.com>
2018-11-04 16:29:45 +0900

    add dead/live lock

--- chapter1/chapter1.go
+++ chapter1/chapter1.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
-var data int
+func bad() {
+	var data int
 
-func main() {
 	go func() {
 		data++
 	}()
@@ -16,4 +19,108 @@ func main() {
 	if data == 0 {
 		fmt.Printf("the value is %v.\n", data)
 	}
+}
+
+func good() {
+	var memoryAccess sync.Mutex
+	var value int
+
+	go func() {
+		memoryAccess.Lock()
+		value++
+		memoryAccess.Unlock()
+	}()
+
+	memoryAccess.Lock()
+	if value == 0 {
+		fmt.Printf("the value is %v.\n", value)
+	} else {
+		fmt.Printf("the value is %v.\n", value)
+	}
+	memoryAccess.Unlock()
+
+}
+
+func deadLock() {
+	type value struct {
+		mu    sync.Mutex
+		value int
+	}
+	var wg sync.WaitGroup
+	printSum := func(v1, v2 *value) {
+		defer wg.Done()
+		v1.mu.Lock()
+		defer v1.mu.Unlock()
+
+		time.Sleep(2 * time.Second)
+		v2.mu.Lock()
+		defer v2.mu.Unlock()
+
+		fmt.Printf("sum=%v\n", v1.value+v2.value)
+	}
+
+	var a, b value
+	wg.Add(2)
+	go printSum(&a, &b)
+	go printSum(&b, &a)
+	wg.Wait()
+}
+
+func liveLock() {
+	cadence := sync.NewCond(&sync.Mutex{})
+	go func() {
+		for range time.Tick(1 * time.Millisecond) {
+			cadence.Broadcast()
+		}
+	}()
+
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
+	}
+
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprint(out, ". Success!")
+			return true
+		}
+		takeStep()
+		atomic.AddInt32(dir, -1)
+		return false
+	}
+
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
+	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() { fmt.Println(out.String()) }()
+		defer walking.Done()
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ {
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
+	}
+
+	var peopleInHallway sync.WaitGroup
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Barbara")
+	peopleInHallway.Wait()
+
+}
+
+func main() {
+	// bad()
+	// good()
+	// deadLock()
+	liveLock()
 }
