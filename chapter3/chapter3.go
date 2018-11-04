9a9c7a982c6f27e7bec009959770e9b132ac1ce6
matsu-chara <matsuy00@gmail.com>
2018-11-05 00:41:36 +0900

    end chapter3

--- chapter3/chapter3.go
+++ chapter3/chapter3.go
@@ -1,7 +1,110 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
 
+func waitSharedMemory() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
+}
+
+func blockGoroutine() {
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+
+	const numGoroutines = 134
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+}
+
+func BenchmarkContextSwitch(b *testing.B) {
+	var wg sync.WaitGroup
+	begin := make(chan struct{})
+	c := make(chan struct{})
+
+	var token struct{}
+	sender := func() {
+		defer wg.Done()
+		<-begin
+		for i := 0; i < b.N; i++ {
+			c <- token
+		}
+	}
+	receiver := func() {
+		defer wg.Done()
+		<-begin
+		for i := 0; i < b.N; i++ {
+			<-c
+		}
+	}
+
+	wg.Add(2)
+	go sender()
+	go receiver()
+	b.StartTimer()
+	close(begin)
+	wg.Wait()
+}
+
+func channelCompile() {
+	// var dataChan chan interface{}
+	// dataChan = make(chan interface{})
+
+	// var writeStream chan<- interface()
+	// writeStream = dataChan
+	// <-writeStream
+
+	// var readStream <-chan interface()
+	// readStream := dataChan
+	// readStream <- struct{}{}
+}
+
+func selectExample() {
+	c1 := make(chan interface{})
+	close(c1)
+	c2 := make(chan interface{})
+	close(c2)
+
+	var c1Count, c2Count int
+	for i := 1000; i >= -0; i-- {
+		select {
+		case <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		}
+	}
+
+	fmt.Printf("c1Count: %d\nc2Count: %d\n", c1Count, c2Count)
+}
 
 func main() {
-
+	// waitSharedMemory()
+	// blockGoroutine()
+	// BenchmarkContextSwitch()
 }
